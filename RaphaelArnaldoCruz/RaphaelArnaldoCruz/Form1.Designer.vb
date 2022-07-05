abea5c3c3a5ca502c5a6fc9bf9b8047c940a8dec
Raphael Arnaldo Cruz <53103201+INN0C0DE@users.noreply.github.com>
2022-07-05 16:39:56 +0800

    Add files via upload

---
+++ RaphaelArnaldoCruz/RaphaelArnaldoCruz/Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.q1 = New System.Windows.Forms.TextBox()
+        Me.q2 = New System.Windows.Forms.TextBox()
+        Me.q3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.qa = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Poppins", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(96, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(345, 48)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Quiz Average Calculator"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Poppins", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(99, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 28)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "QUIZ 1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Poppins", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(241, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 28)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "QUIZ 2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Poppins", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(379, 94)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 28)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "QUIZ 3"
+        '
+        'q1
+        '
+        Me.q1.Font = New System.Drawing.Font("Poppins", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.q1.Location = New System.Drawing.Point(79, 125)
+        Me.q1.Name = "q1"
+        Me.q1.Size = New System.Drawing.Size(99, 36)
+        Me.q1.TabIndex = 4
+        Me.q1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'q2
+        '
+        Me.q2.Font = New System.Drawing.Font("Poppins", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.q2.Location = New System.Drawing.Point(220, 125)
+        Me.q2.Name = "q2"
+        Me.q2.Size = New System.Drawing.Size(99, 36)
+        Me.q2.TabIndex = 5
+        Me.q2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'q3
+        '
+        Me.q3.Font = New System.Drawing.Font("Poppins", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.q3.Location = New System.Drawing.Point(358, 125)
+        Me.q3.Name = "q3"
+        Me.q3.Size = New System.Drawing.Size(99, 36)
+        Me.q3.TabIndex = 6
+        Me.q3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button1.Font = New System.Drawing.Font("Poppins", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(195, 202)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(156, 45)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "COMPUTE"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'qa
+        '
+        Me.qa.Font = New System.Drawing.Font("Poppins", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.qa.Location = New System.Drawing.Point(220, 280)
+        Me.qa.Name = "qa"
+        Me.qa.Size = New System.Drawing.Size(104, 36)
+        Me.qa.TabIndex = 8
+        Me.qa.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Poppins", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(75, 285)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(144, 28)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "QUIZ AVERAGE IS:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Poppins", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(318, 359)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(164, 23)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Raphael Arnaldo S. Cruz"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Poppins", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(484, 359)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(55, 23)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "BSIT 2A"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(537, 380)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.qa)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.q3)
+        Me.Controls.Add(Me.q2)
+        Me.Controls.Add(Me.q1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Quiz Average Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents q1 As System.Windows.Forms.TextBox
+    Friend WithEvents q2 As System.Windows.Forms.TextBox
+    Friend WithEvents q3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents qa As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ RaphaelArnaldoCruz/RaphaelArnaldoCruz/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private db1, db2, db3, db4 As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub q1_TextChanged(sender As Object, e As EventArgs) Handles q1.TextChanged
+        db1 = q1.Text
+
+    End Sub
+
+    Private Sub q2_TextChanged(sender As Object, e As EventArgs) Handles q2.TextChanged
+        db2 = q2.Text
+
+    End Sub
+
+    Private Sub q3_TextChanged(sender As Object, e As EventArgs) Handles q3.TextChanged
+        db3 = q3.Text
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        db4 = (db1 + db2 + db3) / 3
+        qa.Text = db4
+
+    End Sub
+End Class
